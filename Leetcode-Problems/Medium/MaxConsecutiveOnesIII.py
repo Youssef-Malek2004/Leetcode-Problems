fb737408abf76f209d23c5ad185e217ea032c7ae
Youssef-Malek2004 <161763492+Youssef-Malek2004@users.noreply.github.com>
2026-07-17 20:42:03 +0300

    Add Max Consecutive Ones III using sliding window

---
+++ Leetcode-Problems/Medium/MaxConsecutiveOnesIII.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestOnes(self, nums: list[int], k: int) -> int:
+        left = 0
+        flipped = 0
+
+        longest = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                flipped += 1
+
+            while flipped > k:
+                if nums[left] == 0:
+                    flipped -= 1
+
+                left += 1
+
+            longest = max(longest, (right - left + 1))
+
+        return longest
+
+        
